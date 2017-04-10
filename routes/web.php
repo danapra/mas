78eae2a003ca3e77a006ee5e1ce829b9dd6df4e6
edwin_mp <eputra35@gmail.com>
2017-04-10 17:51:47 +0700

    add custom menu & view

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix' => 'admin'], function ()
+{
+	Route::get('surat/masuk', ['as' => 'surat.masuk', 'uses' => 'SuratMasukController@index']);
 });
