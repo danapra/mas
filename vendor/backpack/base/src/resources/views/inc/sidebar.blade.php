e6795af0b93227861ce67faba63f18e20657efe7
edwin_mp <eputra35@gmail.com>
2017-04-20 10:18:37 +0700

    first

--- vendor/backpack/base/src/resources/views/inc/sidebar.blade.php
+++ vendor/backpack/base/src/resources/views/inc/sidebar.blade.php
@@ -20,7 +20,6 @@
           <!-- ==== Recommended place for admin menu items ==== -->
           <!-- ================================================ -->
           <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-          <li><a href="{{ route('surat.masuk') }}"><i class="fa fa-dashboard"></i> <span>Surat masuk</span></a></li>
 
 
           <!-- ======================================= -->
